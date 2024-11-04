3f43eb09d3268bd8931911974711bbf698bb8c2f
uzimasam <uzimasamuel1@gmail.com>
2024-11-04 03:53:33 +0300

    Remove Additional page and update application URLs in configuration

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -25,7 +25,6 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
